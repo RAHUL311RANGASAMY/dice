13d657b1a3f5288aaf5acbe74b54664305a207ae
RAHUL311RANGASAMY <157510957+RAHUL311RANGASAMY@users.noreply.github.com>
2024-07-16 11:38:01 +0530

    files upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var random=Math.floor(Math.random()*6)+1;
+var randomdiceimages="dice"+random+".png";
+var randomimagessources="images/"+randomdiceimages;
+//logic 5-6 lines
+var images1=document.querySelectorAll("img")[0];
+images1.setAttribute("src",randomimagessources);
+
+var random2=Math.floor(Math.random()*6)+1;
+var randomimages2="images/dice"+random2+".png";
+var images2=document.querySelectorAll("img")[1];
+images2.setAttribute("src",randomimages2);
+
+if(random>random2){
+    document.querySelector("h1").innerHTML="Player 1 wins!";
+}
+else if(random<random2){
+    document.querySelector("h1").innerHTML="Player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML="Draw!";
+}
+
+var images1=docum.quert(fgf)
